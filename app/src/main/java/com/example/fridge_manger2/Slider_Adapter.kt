3bd13ece1b2912b4eb49f1af1b216b5816bf5074
alicia <huang728822@yahoo.com>
2021-08-28 16:33:53 +0800

    新增onboarding功能 目前設定第一次啟動執行 圖片用我之前找到的例子代替

---
+++ app/src/main/java/com/example/fridge_manger2/Slider_Adapter.kt
@@ -0,0 +1,55 @@
+package com.example.fridge_manger2
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.viewpager.widget.PagerAdapter
+
+class Slider_Adapter: PagerAdapter{
+
+    private val context:Context
+
+    private lateinit var layoutinflater:LayoutInflater
+
+    constructor(context: Context) : super() {
+        this.context = context
+    }
+
+    //content
+    private val images:ArrayList<Int> = arrayListOf(R.drawable.add_missing_place,R.drawable.make_a_call,
+        R.drawable.search_place,R.drawable.sit_back_and_relax)
+    private val features:ArrayList<Int> = arrayListOf(R.string.feature1,R.string.feature2,R.string.feature3,R.string.feature4)
+    private val descriptions:ArrayList<Int> = arrayListOf(R.string.descript1,R.string.descript2,R.string.descript3,R.string.descript4)
+
+
+    override fun getCount(): Int {
+        return features.size
+    }
+
+    override fun isViewFromObject(view: View, `object`: Any): Boolean {
+        return view==`object`
+    }
+
+    override fun instantiateItem(container: ViewGroup, position: Int): Any {
+        layoutinflater=context.getSystemService(Context.LAYOUT_INFLATER_SERVICE)as LayoutInflater
+        val view:View=layoutinflater.inflate(R.layout.slides_layout,container,false)
+
+        val image:ImageView=view.findViewById(R.id.slider_image)
+        val feature:TextView=view.findViewById(R.id.feature)
+        val descripton:TextView=view.findViewById(R.id.descript)
+
+        image.setImageResource(images[position])
+        feature.setText(features[position])
+        descripton.setText(descriptions[position])
+
+        container.addView(view)
+        return view
+    }
+
+    override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
+        container.removeView(`object` as View?)
+    }
+}
