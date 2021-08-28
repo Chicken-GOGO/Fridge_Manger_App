31812aeeb2e9186d660dcba576b83a08c0435f7a
alicia <huang728822@yahoo.com>
2021-08-28 16:33:09 +0800

    新增onboarding功能 目前設定第一次啟動執行 圖片用我之前找到的例子代替

--- app/src/main/java/com/example/fridge_manger2/MainActivity.kt
+++ app/src/main/java/com/example/fridge_manger2/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.fridge_manger2
 
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
@@ -18,11 +21,31 @@ class MainActivity : AppCompatActivity() {
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
 
+    lateinit var onboardingscreen :SharedPreferences//lateinit:確定變數會被初始化 只能被用在會改變的屬性上
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+//讓Onboarding只顯示一次
+        onboardingscreen=getSharedPreferences("onboardingscreen", MODE_PRIVATE)
+        var isfirsttime:Boolean=onboardingscreen.getBoolean("firsttime",true)
+        if(isfirsttime)
+        {
+
+            var editor:SharedPreferences.Editor=onboardingscreen.edit()
+            editor.putBoolean("firsttime",false)
+            editor.commit()
+
+            var intent:Intent= Intent(this,Onboarding::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+
+
 
         setSupportActionBar(binding.appBarMain.toolbar)
 
